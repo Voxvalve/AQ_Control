2769f4754725b3fc716064c99069734314458de0
Voxvalve <rene.viper999@gmail.com>
2014-03-10 12:09:34 +0100

    getTemp is now working with OneWire.

--- AQ_Control.ino
+++ AQ_Control.ino
@@ -6,15 +6,60 @@
   3.) Turn off light.
   ---------------------
 */
+#define DS18S20_ID 0x10
+#define DS18B20_ID 0x28
+#include <OneWire.h>
 
 int intRead; // int to identify command chosen.
+int pin_number = 2;
 float temp; // Temprature of the water.
-bool light = 0; // lights off by default.
+byte i;
+byte present = 0;
+byte data[12];
+byte addr[8];
+boolean light = 0; // lights off by default.
+OneWire ds(pin_number); // Select pin wher DS18B22 is connected.
 
-void getTemp() {
-  temp = 13.37; // test float for temprature wright out.
+
+// Function to get temperature.
+boolean getTemp(){
+ 
+ //If device not found on addr.
+ if (!ds.search(addr)) {
+   ds.reset_search(); // Clear search state.
+   return false;
+ }
+ 
+ if (OneWire::crc8( addr, 7) != addr[7]) {
+   return false;
+ }
+ 
+ if (addr[0] != DS18S20_ID && addr[0] != DS18B20_ID) {
+   return false;
+ }
+ ds.reset(); // Reset cycle to see if device still present.
+ ds.select(addr); // Select rom after reset.
+ 
+ ds.write(0x44, 1); // Start conversion
+ 
+ delay(850); // Wait some time.
+ present = ds.reset(); // Check if DS18B22 is present.
+ ds.select(addr); // Select rom command addr 8.
+ 
+ // Issue Read scratchpad command
+ ds.write(0xBE);
+ 
+ // Receive 9 bytes
+ for ( i = 0; i < 9; i++) {
+   data[i] = ds.read();
+ }
+ 
+ // Calculate temperature value
+ temp = ( (data[1] << 8) + data[0] )*0.0625;
+ return true;
 }
 
+// Control Lights ON/OFF.
 void lights() {
   if (light == 0) {
     // Turn on light.
